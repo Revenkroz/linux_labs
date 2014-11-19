ec69dc3e244207d66e0cc9ebca4345b9c186d522
revenkroz <observerolwell@gmail.com>
2014-11-19 22:53:01 +0400

    +lab4

--- lab4/lab4a.c
+++ lab4/lab4a.c
@@ -18,7 +18,7 @@ int main()
         perror("pipe");
         exit(1);
     }
-
+    printf("Starting threads...\n");
     pthread_create(&rd, NULL, reader, NULL);
     pthread_create(&wr, NULL, writer, NULL);
 
